528a700868ad07a86c620163f07b69b52171ffc6
BerendMB <berendmeesse@gmail.com>
2024-02-19 17:52:49 +0000

    Code for inverse matrix function for programming assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a list of the functions used for calculating and caching inverse matrices
 makeCacheMatrix <- function(x = matrix()) {
 inv <- NULL
 set <- function(y) {
@@ -14,6 +10,9 @@ setinverse <- function(inverse) inv <<- inverse
 getinverse <- function() inv
 list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
+
+## This second function retrieves the inverse matrix if it has already been made before and 
+## caches the inverse matrix if it has not been made before
 
 cacheSolve <- cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
